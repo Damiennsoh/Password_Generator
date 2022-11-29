d5cb16f35b449b64943c82a8ac08d0a6c57815a4
Damiennsoh <psycaro20@gmail.com>
2022-11-29 23:33:00 +0530

    updated javascript codes and created a screenshot directory

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Password_Generator
 Generate a password when the button is clicked
 
-# Description
+## Description
 create an application that an employee can use to generate a random password based on criteria they’ve selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code. It will have a clean and polished user interface that is responsive, ensuring that it adapts to multiple screen sizes.
 
 ## Installation
--- starter/script.js
+++ starter/script.js
@@ -88,19 +88,59 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+function getPasswordLength() {
+  passwordLength = 0;
+  do {
+    passwordLength = parseInt(prompt("Choose the length of your password.\n It has to be at least 10 characters but no more than 64."));
+    if (!isFinite(passwordLength) || passwordLength < 10 || passwordLength > 64) {
+      alert("password should be at least 10 characters but no more than 64.");
+    }
+  } while (!isFinite(passwordLength) || passwordLength < 10 || passwordLength > 64);
+  return passwordLength;
+}
 // Function to prompt user for password options
-function getPasswordOptions() {
+function getCharOptions() {
+  mixOfChosenCharacters = [];
+  do {
+    if (confirm("Would you like lowercase characters in your password?")) {
+      mixOfChosenCharacters = mixOfChosenCharacters.concat(lowerCasedCharacters);
+    }
+    if (confirm("Would you like Uppercase characters in your password?")) {
+      mixOfChosenCharacters = mixOfChosenCharacters.concat(upperCasedCharacters);
+    }
+    if (confirm("Would you like Numbers in your password?")) {
+      mixOfChosenCharacters = mixOfChosenCharacters.concat(numericCharacters);
+    }
+    if (confirm("Would you like Special characters ($@%&*, etc) in your password?")) {
+      mixOfChosenCharacters = mixOfChosenCharacters.concat(specialCharacters);
+    }
 
+    if (mixOfChosenCharacters.length == 0) {
+      alert("You must choose at least one character type for your password!");
+    }
+  } while (mixOfChosenCharacters.length == 0)// repeat if no character type was selected
+  return mixOfChosenCharacters;
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The m
+}
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  return arr[getRandomIntInclusive(0, (arr.length - 1))];
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+  pass = "";
+  passLength = getPasswordLength()
+  passwordChosenCharacters = getCharOptions();
+  for (i = 0; i < passLength; i++) {
+    pass += getRandom(passwordChosenCharacters)
+  }
+  return pass;
 }
 
 // Get references to the #generate element
@@ -109,8 +149,7 @@ var generateBtn = document.querySelector('#generate');
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
+  var passwordText = document.getElementById('password');
   passwordText.value = password;
 }
 
